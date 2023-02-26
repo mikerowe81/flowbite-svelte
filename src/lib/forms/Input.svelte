853dd681b5ab5df4981093287796006cd765023b
Michael Rowe <michael.rowe@abc.edu>
2023-02-25 19:16:12 -0500

    Force autofocus on input

--- src/lib/forms/Input.svelte
+++ src/lib/forms/Input.svelte
@@ -8,7 +8,7 @@
 <script lang="ts">
   import Wrapper from '$lib/utils/Wrapper.svelte';
   import classNames from 'classnames';
-  import { getContext } from 'svelte';
+  import {getContext, onMount} from 'svelte'
   import type { InputType } from '../types';
 
   export let type: InputType = 'text';
@@ -16,6 +16,14 @@
   export let size: FormSizeType | undefined = undefined;
   export let defaultClass: string = 'block w-full disabled:cursor-not-allowed disabled:opacity-50';
   export let color: 'base' | 'green' | 'red' = 'base';
+
+  onMount(() => {
+    if(inputRef && $$props.autofocus) {
+      setTimeout(() => {
+        inputRef.focus()
+      }, 50)
+    }
+  })
 
   const borderClasses = {
     base: 'border-gray-300 dark:border-gray-600',
@@ -79,6 +87,7 @@
     );
   }
   let floatClass = 'flex absolute inset-y-0 items-center text-gray-500 dark:text-gray-400';
+  let inputRef: HTMLInputElement
 </script>
 
 <Wrapper class="relative w-full" show={$$slots.left || $$slots.right}>
@@ -89,6 +98,7 @@
     <input
       {...$$restProps}
       bind:value
+      bind:this={inputRef}
       on:blur
       on:change
       on:click

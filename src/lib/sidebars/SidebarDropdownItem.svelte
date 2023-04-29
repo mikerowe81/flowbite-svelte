4f43ec0e1471326db4dd56279f62873ff00a17db
Michael Rowe <michael.rowe@abc.edu>
2023-04-29 10:58:01 -0400

    Tweaked Sidebar Items Hover

--- src/lib/sidebars/SidebarDropdownItem.svelte
+++ src/lib/sidebars/SidebarDropdownItem.svelte
@@ -2,11 +2,11 @@
   import classNames from 'classnames';
 
   export let aClass: string =
-    'flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700';
+    'flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700';
   export let href: string = '';
   export let label: string = '';
   export let activeClass: string =
-    'flex items-center p-2 pl-11 text-base font-normal text-gray-900 bg-gray-200 dark:bg-gray-700 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
+    'flex items-center p-2 pl-11 text-base font-normal text-gray-900 bg-gray-200 dark:bg-gray-700 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700';
   export let active: boolean = false;
 </script>
 
--- src/lib/sidebars/SidebarItem.svelte
+++ src/lib/sidebars/SidebarItem.svelte
@@ -2,12 +2,12 @@
   import classNames from 'classnames';
 
   export let aClass: string =
-    'flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
+    'flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700';
   export let href: string = '';
   export let label: string = '';
   export let spanClass: string = 'ml-3';
   export let activeClass: string =
-    'flex items-center p-2 text-base font-normal text-gray-900 bg-gray-200 dark:bg-gray-700 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700';
+    'flex items-center p-2 text-base font-normal text-gray-900 bg-gray-200 dark:bg-gray-700 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700';
   export let active: boolean = false;
 </script>
 
